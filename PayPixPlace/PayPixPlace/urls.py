e4b917f77709c9513e224062d23a8c5b0cf16a14
Célien Donzé <celien.donze@he-arc.ch>
2019-03-06 10:36:06 +0100

    create login page

--- PayPixPlace/PayPixPlace/urls.py
+++ PayPixPlace/PayPixPlace/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.contrib.auth import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('login/', views.LoginView.as_view(), name='login'),
 ]
