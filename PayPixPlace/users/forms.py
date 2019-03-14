5e66078c98fbb0cdcbcd83c1c277a8c83fba004d
Alexander Wohlfahrt <alexander.wohlfahrt@he-arc.ch>
2019-03-14 16:34:06 +0100

    Create custom login for form customization (crispy)

--- PayPixPlace/users/forms.py
+++ PayPixPlace/users/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Layout, Row, Column
 from crispy_forms.bootstrap import PrependedText, StrictButton
@@ -16,6 +16,7 @@ class RegisterForm(UserCreationForm):
         super(RegisterForm, self).__init__(*args, **kwargs)
         self.helper = FormHelper()
         self.helper.form_method = 'POST'
+        self.helper.form_action = './'
         self.helper.layout = Layout(
             PrependedText('username', '<i class="fas fa-user"></i>'),
             Row(
@@ -45,4 +46,21 @@ class UpdateForm(forms.ModelForm):
             'email',
             'first_name',
             'last_name'
+        )
+
+class LoginForm(AuthenticationForm):
+
+    class Meta:
+        model = User
+        fields = ( 'username', 'password' )
+
+    def __init__(self, *args, **kwargs):
+        super(LoginForm, self).__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_method = 'POST'
+        self.helper.form_action = './'
+        self.helper.layout = Layout(
+            PrependedText('username', '<i class="fas fa-user"></i>'),
+            PrependedText('password', '<i class="fas fa-key"></i>'),
+            StrictButton('Login', type='submit', css_class='btn-outline-info mb-3'),
         )
