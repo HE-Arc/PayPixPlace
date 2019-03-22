88ab140451a9239edc95c85a4edd79c69023b276
Alexander Wohlfahrt <alexander.wohlfahrt@he-arc.ch>
2019-03-22 23:09:49 +0100

    Continue design of buy color modal

--- PayPixPlace/paypixplaceapp/static/paypixplaceapp/buy_with_pix.js
+++ PayPixPlace/paypixplaceapp/static/paypixplaceapp/buy_with_pix.js
@@ -1,30 +1,32 @@
-document.getElementById("btnFixColor").addEventListener("click", function(){
-    $.ajax({
-        type: "POST",
-        url: "/buy/0",
-        data: {
-            csrfmiddlewaretoken: window.CSRF_TOKEN,
-            "hex": document.getElementById("fix_color").value,
-        },
-        dataType: "json",
-        success: function (data) {
-            console.log(data);
-            document.getElementById("userPix").innerHTML = data.UserPix;
-        }
+$(document).ready(function() {
+    document.getElementById("btnFixColor").addEventListener("click", function(){
+        $.ajax({
+            type: "POST",
+            url: "/buy/0",
+            data: {
+                csrfmiddlewaretoken: window.CSRF_TOKEN,
+                "hex": document.getElementById("fix_color").value,
+            },
+            dataType: "json",
+            success: function (data) {
+                console.log(data);
+                document.getElementById("userPix").innerHTML = data.UserPix;
+            }
+        });
     });
-});
 
-document.getElementById("btnRandomColor").addEventListener("click", function(){
-    $.ajax({
-        type: "POST",
-        url: "/buy/2",
-        data: {
-            csrfmiddlewaretoken: window.CSRF_TOKEN,
-        },
-        dataType: "json",
-        success: function (data) {
-            console.log(data);
-            document.getElementById("userPix").innerHTML = data.UserPix;
-        }
+    document.getElementById("btnRandomColor").addEventListener("click", function(){
+        $.ajax({
+            type: "POST",
+            url: "/buy/2",
+            data: {
+                csrfmiddlewaretoken: window.CSRF_TOKEN,
+            },
+            dataType: "json",
+            success: function (data) {
+                console.log(data);
+                document.getElementById("userPix").innerHTML = data.UserPix;
+            }
+        });
     });
 });
